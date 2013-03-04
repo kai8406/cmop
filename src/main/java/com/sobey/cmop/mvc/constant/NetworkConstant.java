7de9e05c91501b1b27d2ba75e6c2f74d866f95db
liukai <kai8406@gmail.com>
2013-03-04 13:03:51 +0800

    增加几个network的常量.

--- src/main/java/com/sobey/cmop/mvc/constant/NetworkConstant.java
+++ src/main/java/com/sobey/cmop/mvc/constant/NetworkConstant.java
@@ -16,11 +16,15 @@ public class NetworkConstant {
 	 * ESG的协议.<br>
 	 * 注意:返回的map key和value均为String类型
 	 * 
+	 * <pre>
+	 * TCP,UDP,ICMP,HTTP,HTTPS, SSH, DNS, MYSQL;
+	 * </pre>
 	 * 
 	 * @author liukai
 	 * 
 	 */
 	public enum EsgProtocol {
+
 		TCP, UDP, ICMP, HTTP, HTTPS, SSH, DNS, MYSQL;
 
 		public static final Map<String, String> map = Maps.newLinkedHashMap();
@@ -31,6 +35,137 @@ public class NetworkConstant {
 
 			}
 		}
+	}
+
+	/**
+	 * 网络资源的协议类型
+	 * 
+	 * <pre>
+	 * HTTP,HTTPS,TCP,SSL,ALL
+	 * </pre>
+	 * 
+	 * @author liukai
+	 * 
+	 */
+	public enum Protocol {
+
+		HTTP, HTTPS, TCP, SSL, ALL;
+
+		public static final Map<String, String> map = Maps.newLinkedHashMap();
+		static {
+			for (Protocol e : Protocol.values()) {
+
+				map.put(e.name(), e.name());
+
+			}
+		}
+	}
+
+	/**
+	 * 计算资源类型.
+	 * <p>
+	 * true :1-保持<br>
+	 * false:2-不保持<br>
+	 * <p>
+	 * 
+	 * @author liukai
+	 * 
+	 */
+	public enum KeepSession implements CommonEnum {
+		保持(1), 不保持(2);
+
+		private int code;
+
+		private KeepSession(int code) {
+			this.code = code;
+		}
+
+		public static final Map<Integer, String> map = Maps.newLinkedHashMap();
+		static {
+			for (KeepSession e : KeepSession.values()) {
+
+				map.put(e.code, e.name());
+
+			}
+		}
+
+		public static final Map<String, String> mapKeyStr = Maps.newLinkedHashMap();
+		static {
+			for (KeepSession e : KeepSession.values()) {
+
+				mapKeyStr.put(String.valueOf(e.code), e.name());
+
+			}
+		}
+
+		public static String get(Integer code) {
+			return map.get(code);
+		}
+
+		@Override
+		public Integer toInteger() {
+			return this.code;
+		}
+
+		@Override
+		public String toString() {
+			return String.valueOf(this.code);
+		}
+
+	}
+
+	/**
+	 * 域名类型
+	 * <p>
+	 * 1-GSLB<br>
+	 * 2-A<br>
+	 * 3-CNAME<br>
+	 * <p>
+	 * 
+	 * @author liukai
+	 * 
+	 */
+	public enum DomainType implements CommonEnum {
+		GSLB(1), A(2), CNAME(3);
+
+		private int code;
+
+		private DomainType(int code) {
+			this.code = code;
+		}
+
+		public static final Map<Integer, String> map = Maps.newLinkedHashMap();
+		static {
+			for (DomainType e : DomainType.values()) {
+
+				map.put(e.code, e.name());
+
+			}
+		}
+
+		public static final Map<String, String> mapKeyStr = Maps.newLinkedHashMap();
+		static {
+			for (DomainType e : DomainType.values()) {
+
+				mapKeyStr.put(String.valueOf(e.code), e.name());
+
+			}
+		}
+
+		public static String get(Integer code) {
+			return map.get(code);
+		}
+
+		@Override
+		public Integer toInteger() {
+			return this.code;
+		}
+
+		@Override
+		public String toString() {
+			return String.valueOf(this.code);
+		}
+
 	}
 
 }

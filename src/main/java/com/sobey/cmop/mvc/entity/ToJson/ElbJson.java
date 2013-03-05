587eb12aa444b3ac9c3134a728bec170dccd5bb3
liukai <kai8406@gmail.com>
2013-03-05 17:30:27 +0800

    新增ElbJson实体类.用于封装networkelbitem

---
+++ src/main/java/com/sobey/cmop/mvc/entity/ToJson/ElbJson.java
@@ -0,0 +1,67 @@
+package com.sobey.cmop.mvc.entity.ToJson;
+
+/**
+ * NetworkElbItem entity. @author MyEclipse Persistence Tools
+ */
+public class ElbJson implements java.io.Serializable {
+
+	private Integer id;
+	private String identifier;
+	private String keepSession;
+	private String virtualIp;
+	private String elbPortItems;
+
+	/**
+	 * 关联Compute组合信息 :实例标识符(内网IP)
+	 */
+	private String relationCompute;
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getIdentifier() {
+		return identifier;
+	}
+
+	public void setIdentifier(String identifier) {
+		this.identifier = identifier;
+	}
+
+	public String getKeepSession() {
+		return keepSession;
+	}
+
+	public void setKeepSession(String keepSession) {
+		this.keepSession = keepSession;
+	}
+
+	public String getVirtualIp() {
+		return virtualIp;
+	}
+
+	public void setVirtualIp(String virtualIp) {
+		this.virtualIp = virtualIp;
+	}
+
+	public String getElbPortItems() {
+		return elbPortItems;
+	}
+
+	public void setElbPortItems(String elbPortItems) {
+		this.elbPortItems = elbPortItems;
+	}
+
+	public String getRelationCompute() {
+		return relationCompute;
+	}
+
+	public void setRelationCompute(String relationCompute) {
+		this.relationCompute = relationCompute;
+	}
+
+}

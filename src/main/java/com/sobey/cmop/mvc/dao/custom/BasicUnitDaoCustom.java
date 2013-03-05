288e20bfba65f0891976149b65241c95c976791c
liukai <kai8406@gmail.com>
2013-03-05 17:29:08 +0800

    新增一个根据resourcesId获得compute的自定义Dao方法

--- src/main/java/com/sobey/cmop/mvc/dao/custom/BasicUnitDaoCustom.java
+++ src/main/java/com/sobey/cmop/mvc/dao/custom/BasicUnitDaoCustom.java
@@ -1,5 +1,7 @@
 package com.sobey.cmop.mvc.dao.custom;
 
+import java.util.List;
+
 /**
  * 基础设施模块(Compute,ES3,ELB等)的自定义Dao
  * 
@@ -17,5 +19,14 @@ public interface BasicUnitDaoCustom {
 	 *            存储空间ID
 	 */
 	public void saveComputeAndStorageRelevance(String computeId, Integer storageItemId);
+
+	/**
+	 * 获得指定用户创建的审批通过的实例ComputeList,并且该实例没有被其它ELB关联过,即elb_id = null<br>
+	 * 
+	 * @param userId
+	 * @return
+	 */
+	@SuppressWarnings("rawtypes")
+	public List getComputeItemListByResourcesId(Integer userId);
 
 }
--- src/main/java/com/sobey/cmop/mvc/dao/custom/BasicUnitDaoCustomImp.java
+++ src/main/java/com/sobey/cmop/mvc/dao/custom/BasicUnitDaoCustomImp.java
@@ -1,5 +1,7 @@
 package com.sobey.cmop.mvc.dao.custom;
 
+import java.util.List;
+
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.persistence.Query;
@@ -7,6 +9,8 @@ import javax.persistence.Query;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Repository;
+
+import com.sobey.cmop.mvc.constant.ResourcesConstant;
 
 @Repository
 public class BasicUnitDaoCustomImp implements BasicUnitDaoCustom {
@@ -29,6 +33,17 @@ public class BasicUnitDaoCustomImp implements BasicUnitDaoCustom {
 
 		query.executeUpdate();
 
+	}
+
+	@SuppressWarnings("rawtypes")
+	@Override
+	public List getComputeItemListByResourcesId(Integer userId) {
+		String sqlString = "select t2.* from resources t1 inner join compute_item t2 on t1.service_id = t2.id" + " where t1.service_type = " + ResourcesConstant.ServiceType.PCS
+				+ " or t1.service_type = " + ResourcesConstant.ServiceType.ECS + "  and elb_id is null  and t1.user_id = ?";
+
+		logger.info(sqlString);
+
+		return em.createNativeQuery(sqlString).setParameter(1, userId).getResultList();
 	}
 
 }

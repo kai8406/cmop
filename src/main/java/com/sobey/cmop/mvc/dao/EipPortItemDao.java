d4b0fb3cdf59c3f5d6c0d32d4c664f803d479089
liukai <kai8406@gmail.com>
2013-03-06 16:55:41 +0800

    泛型赋值错误.

--- src/main/java/com/sobey/cmop/mvc/dao/EipPortItemDao.java
+++ src/main/java/com/sobey/cmop/mvc/dao/EipPortItemDao.java
@@ -3,12 +3,14 @@ package com.sobey.cmop.mvc.dao;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.repository.PagingAndSortingRepository;
 
+import com.sobey.cmop.mvc.entity.EipPortItem;
+
 /**
  * EIP端口映射明细 对象 EipPortItem 的Dao interface.
  * 
  * @author liukai
  * 
  */
-public interface EipPortItemDao extends PagingAndSortingRepository<EipPortItemDao, Integer>, JpaSpecificationExecutor<EipPortItemDao> {
+public interface EipPortItemDao extends PagingAndSortingRepository<EipPortItem, Integer>, JpaSpecificationExecutor<EipPortItem> {
 
 }

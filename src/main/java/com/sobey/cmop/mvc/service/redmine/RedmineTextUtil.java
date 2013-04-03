d2fd24906b207b973a9b03c4ad12e52c9d672acc
liukai <kai8406@gmail.com>
2013-04-03 14:27:33 +0800

    Redmine中增加EIP的公网IP字段的显示.

--- src/main/java/com/sobey/cmop/mvc/service/redmine/RedmineTextUtil.java
+++ src/main/java/com/sobey/cmop/mvc/service/redmine/RedmineTextUtil.java
@@ -184,6 +184,7 @@ public class RedmineTextUtil {
 
 			for (NetworkEipItem eipItem : eipItems) {
 				content.append("标识符:").append(BLANK).append(eipItem.getIdentifier()).append(NEWLINE);
+				content.append("IP地址:").append(BLANK).append(eipItem.getIpAddress()).append(NEWLINE);
 				content.append("ISP运营商:").append(BLANK).append(NetworkConstant.ISPType.get(eipItem.getIspType())).append(NEWLINE);
 
 				if (eipItem.getComputeItem() != null) {

f50a65812ae585ebd385d92fd6c7b4d9eaac093c
liukai <kai8406@gmail.com>
2013-05-20 10:51:47 +0800

    提供数字的加减乘除的工具类.

---
+++ src/main/java/com/sobey/framework/utils/MathsUtil.java
@@ -0,0 +1,97 @@
+package com.sobey.framework.utils;
+
+import java.math.BigDecimal;
+
+/**
+ * 提供数字的加减乘除的方法.
+ * 
+ * @author liukai
+ * 
+ */
+public class MathsUtil {
+
+	/**
+	 * 默认除法运算精度(小数点后2位数)
+	 */
+	private static final int DEF_DIV_SCALE = 2;
+
+	/**
+	 * 
+	 * 提供精确的加法运算.
+	 * 
+	 * @param v1
+	 *            被加数
+	 * @param v2
+	 *            加数
+	 * @return 两个参数的和
+	 */
+	public static double add(double v1, double v2) {
+		BigDecimal b1 = new BigDecimal(Double.toString(v1));
+		BigDecimal b2 = new BigDecimal(Double.toString(v2));
+		return b1.add(b2).doubleValue();
+	}
+
+	/**
+	 * 提供精确的减法运算.
+	 * 
+	 * @param v1
+	 *            被减数
+	 * @param v2
+	 *            减数
+	 * @return 两个参数的差
+	 */
+	public static double sub(double v1, double v2) {
+		BigDecimal b1 = new BigDecimal(Double.toString(v1));
+		BigDecimal b2 = new BigDecimal(Double.toString(v2));
+		return b1.subtract(b2).doubleValue();
+	}
+
+	/**
+	 * 提供精确的乘法运算.只保留小数点后两位.
+	 * 
+	 * @param v1
+	 *            被乘数
+	 * @param v2
+	 *            乘数
+	 * @return 两个参数的积
+	 */
+	public static double mul(double v1, double v2) {
+		BigDecimal b1 = new BigDecimal(Double.toString(v1));
+		BigDecimal b2 = new BigDecimal(Double.toString(v2));
+		return b1.multiply(b2).setScale(DEF_DIV_SCALE, BigDecimal.ROUND_HALF_UP).doubleValue();
+
+	}
+
+	/**
+	 * 提供（相对）精确的除法运算.当发生除不尽的情况时,精度为2,以后的数字四舍五入.
+	 * 
+	 * @param v1
+	 *            被除数
+	 * @param v2
+	 *            除数
+	 * @return 两个参数的商
+	 */
+	public static double div(double v1, double v2) {
+		BigDecimal b1 = new BigDecimal(Double.toString(v1));
+		BigDecimal b2 = new BigDecimal(Double.toString(v2));
+		return b1.divide(b2, DEF_DIV_SCALE, BigDecimal.ROUND_HALF_UP).doubleValue();
+	}
+
+	/**
+	 * 提供（相对）精确的除法运算.当发生除不尽的情况时,由scale参数指 定精度,以后的数字四舍五入.
+	 * 
+	 * @param v1
+	 *            被除数
+	 * @param v2
+	 *            除数
+	 * @param scale
+	 *            表示表示需要精确到小数点以后几位.
+	 * @return 两个参数的商
+	 */
+	public static double div(double v1, double v2, int scale) {
+		BigDecimal b1 = new BigDecimal(Double.toString(v1));
+		BigDecimal b2 = new BigDecimal(Double.toString(v2));
+		return b1.divide(b2, scale, BigDecimal.ROUND_HALF_UP).doubleValue();
+	}
+
+}

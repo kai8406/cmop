a8236b42835b3d97dc1c99f4d7bbab94cf5c44d1
liukai <kai8406@gmail.com>
2013-05-22 14:50:29 +0800

    现在注册后直接登录系统了.

--- src/main/java/com/sobey/cmop/mvc/web/account/RegisterController.java
+++ src/main/java/com/sobey/cmop/mvc/web/account/RegisterController.java
@@ -2,6 +2,8 @@ package com.sobey.cmop.mvc.web.account;
 
 import java.util.List;
 
+import org.apache.shiro.SecurityUtils;
+import org.apache.shiro.authc.UsernamePasswordToken;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -40,22 +42,23 @@ public class RegisterController extends BaseController {
 		List<Group> groupList = Lists.newArrayList();
 
 		// 注册用户默认给予 2:apply 申请人 的权限角色.
-
 		groupList.add(comm.accountService.getGroup(AccountConstant.DefaultGroups.apply.toInteger()));
 
 		user.setGroupList(groupList);
 
 		// 给予 2:apply 申请人 的用户类型
-
 		user.setType(AccountConstant.UserTypes.apply.toInteger());
 
 		user.setDepartment(comm.accountService.getDepartment(departmentId));
-
 		comm.accountService.registerUser(user);
 
-		model.addAttribute("message", "请输入登录名和登录密码.");
+		// 登录系统
+		UsernamePasswordToken token = new UsernamePasswordToken();
+		token.setPassword(user.getPlainPassword().toCharArray());
+		token.setUsername(user.getLoginName());
+		SecurityUtils.getSubject().login(token);
 
-		return "account/signIn";
+		return "redirect:/";
 	}
 
 }

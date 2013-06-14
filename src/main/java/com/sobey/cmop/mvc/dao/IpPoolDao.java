8d34435b07da3dcde8b1e4e81b01a74aee22819d
liukai <kai8406@gmail.com>
2013-06-14 16:48:34 +0800

    删除无用的方法.

--- src/main/java/com/sobey/cmop/mvc/dao/IpPoolDao.java
+++ src/main/java/com/sobey/cmop/mvc/dao/IpPoolDao.java
@@ -2,12 +2,9 @@ package com.sobey.cmop.mvc.dao;
 
 import java.util.List;
 
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.repository.PagingAndSortingRepository;
 
-import com.sobey.cmop.mvc.entity.HostServer;
 import com.sobey.cmop.mvc.entity.IpPool;
 
 /**
@@ -18,29 +15,10 @@ import com.sobey.cmop.mvc.entity.IpPool;
  */
 public interface IpPoolDao extends PagingAndSortingRepository<IpPool, Integer>, JpaSpecificationExecutor<IpPool> {
 
-	Page<IpPool> findAllByIpAddressLike(String ipAddress, Pageable pageable);
-
-	Page<IpPool> findAllByPoolTypeAndIpAddressLike(Integer poolType, String ipAddress, Pageable pageable);
-
-	Page<IpPool> findAllByPoolTypeAndStatusAndIpAddressLike(Integer poolType, Integer status, String ipAddress,
-			Pageable pageable);
-
-	Page<IpPool> findAllByStatusAndIpAddressLike(Integer status, String ipAddress, Pageable pageable);
-
-	List<IpPool> findByHostServer(HostServer hostServer);
-
 	IpPool findByIpAddress(String ipAddress);
 
-	IpPool findByIpAddressAndStatus(String ipAddress, Integer ipStatus);
+	List<IpPool> findByPoolTypeAndStatus(Integer poolType, Integer status);
 
-	IpPool findByIpAddressAndVlan_location_alias(String ipAddress, String location);
-
-	List<IpPool> findByPoolType(Integer poolType);
-
-	List<IpPool> findByPoolTypeAndStatus(Integer poolType, Integer ipStatus);
-
-	List<IpPool> findByStatus(Integer ipStatus);
-
-	List<IpPool> findByVlanAliasAndStatus(String vlanAlias, int status);
+	List<IpPool> findByVlanAliasAndStatus(String vlanAlias, Integer status);
 
 }
